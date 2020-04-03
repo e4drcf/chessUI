2e096db413b018794a6a53fd342fa9d39b9de3fd
Ivan Hranat <e4drcf@gmail.com>
2020-04-03 18:21:51 +0300

    fix js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ let openArchive = () => {
     deactivate(playContent);
     archiveTab.classList.add("active-tab");
     playTab.classList.remove("active-tab");
-
 }
 let activate = (content) => {
     content.style.display = "flex";
@@ -19,9 +18,9 @@ let deactivate = (content) => {
     content.style.display = "none";
 }
 let playTab = document.getElementById('play-tab');
-document.getElementById('play-tab').addEventListener('click', openPlay);
+playTab.addEventListener('click', openPlay);
 let archiveTab = document.getElementById('archive-tab');
-document.getElementById('archive-tab').addEventListener('click', openArchive);
+archiveTab.addEventListener('click', openArchive);
 
 let playContent = document.getElementById('play');
 let archiveContent = document.getElementById('archive');
